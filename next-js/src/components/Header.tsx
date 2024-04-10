74124aed6152b3a919f2908099a04ad084313352
kamal sharma <kamalbsr2101@gmail.com>
2024-04-10 10:40:33 +0530

    header section is completed

--- next-js/src/components/Header.tsx
+++ next-js/src/components/Header.tsx
@@ -4,16 +4,20 @@ import Link from 'next/link'
 import React, { useState } from 'react'
 import img from "../../public/images/logo.png";
 import poster from "../../public/images/header-image.png";
+import "@/app/globals.css";
 
 
 export const Header = () => {
     const [lang, setLang] = useState(false);
     return (
         <>
-            {/* background-image: linear-gradient(to top, rgba(0, 0, 0, 0.8) 0, rgba(0, 0, 0, 0) 60%, rgba(0, 0, 0, 0.8) 100%); */}
-            <Image alt='' src={poster} width={1440} height={100} className='absolute bg-gradient-to-t' />
-            <div className="flex flex-col justify-center relative">
-                <div className='flex items-center justify-between px-1 md:px-8 pt-4'>
+            <Image alt='' src={poster} className='absolute min-h-[30rem] h-screen w-full' />
+
+            <div
+                className="flex flex-col relative poster min-h-[30rem] h-screen w-full ">
+
+                <div
+                    className='flex items-center justify-between px-3 md:px-8 pt-4'>
                     <Link href="/">
                         <Image src={img} width={100} height={100} alt='logo' className='' />
                     </Link>
@@ -23,13 +27,29 @@ export const Header = () => {
                         <button className='bg-red-600 text-white py-1 px-2 rounded-md'>Sign In</button>
                     </div>
                 </div>
-                <div className="flex flex-col gap-8 justify-center text-white items-center mt-40">
-                    <h1 className="text-white text-5xl font-extrabold tracking-wide">Unlimited movies, Tv shows and more</h1>
-                    <p className="text-3xl font-semibold">Watch anywhere. Cancel anytime.</p>
-                    <p className="text-xl font-medium">Ready to watch? Enter your email to create or restart your membership.</p>
-                    <div className="flex gap-4">
-                        <input type="text" name="" id="" />
-                        <button className="bg-red-600 px-2 py-1 rounded-md">Get Started</button>
+
+                <div
+                    className="flex flex-col gap-5 justify-center text-white text-center items-center mt-40 md:mt-50 px-2">
+
+                    <h1
+                        className="text-white text-4xl md:text-5xl font-extrabold tracking-wide leading-tight">Unlimited movies, Tv shows and more</h1>
+
+                    <p
+                        className="text-2xl font-semibold"
+                    >
+                        Watch anywhere. Cancel anytime.</p>
+                    <p
+                        className="text-xl font-medium leading-normal"
+                    >Ready to watch? Enter your email to create or restart your membership.</p>
+
+                    <div
+                        className="flex gap-8 md:gap-4 my-2 flex-wrap justify-center items-center"
+                    >
+                        <input type="text" placeholder='Email address'
+                            className='py-2 px-3 rounded-sm border bg-transparent text-2xl ' />
+                        <button
+                            className="bg-red-600 px-3 text-3xl font-normal py-2 rounded-md"
+                        >Get Started  ▷</button>
                     </div>
                 </div>
             </div>
